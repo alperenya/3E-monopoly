134e72ca64765df724e5aa4a7d8a74383e6df63d
kiraci <osmanbugraaydin@gmail.com>
2020-11-25 23:33:38 +0300

    Bot class has been added.

--- src/Bot.java
+++ src/Bot.java
@@ -1,2 +1,22 @@
-public class Bot {
+public class Bot extends Player{
+
+    //properties
+
+    //contructers
+    public Bot(){}
+
+    //methods
+    public Boolean decideBuyingProperty(){
+        return true;
+    }
+    public Boolean decideSellingProperty(){
+        return true;
+    }
+    public Boolean decideBuilding(){
+        return true;
+    }
+    public Boolean trade(){
+        return true;
+    }
+
 }
