ca3214bff44a03d7fbd5cf47e0fb08a2f7f8e3d8
kiraci <osmanbugraaydin@gmail.com>
2020-11-25 23:42:07 +0300

    Cell class has been added.

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,23 @@
 public abstract class Cell {
+
+    //properties
+    private String name;
+    private  Player[] visitors;
+
+    //methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player[] getVisitors() {
+        return visitors;
+    }
+
+    public void setVisitors(Player[] visitors) {
+        this.visitors = visitors;
+    }
 }
