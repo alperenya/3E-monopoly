4daea9bd632906358ddc8db720ba464602c55109
angineeer <32205189+angineeer@users.noreply.github.com>
2020-11-25 23:58:30 +0300

    GameMap class vars and methods added

--- src/GameMap.java
+++ src/GameMap.java
@@ -1,2 +1,21 @@
+import java.util.Hashtable;
+
 public class GameMap {
+    private Cell[] cells;
+    private Card[] cards;
+    private Hashtable colors;
+
+    public GameMap(){}
+    public Cell[] getCells(){ return cells;}
+    public Card[] getCards(){ return cards;} //Was not on the diagram. Newly added
+    public Hashtable getColors(){ return colors;} //Was not on the diagram. Newly added
+
+    public Cell[] getSameColoredProperties(){ return null;}
+    public Player[] getSickPlayers(){ return null;} //getPatientsNames changed to getSickPlayers
+
+    public void addCell(){}
+    public void addCards(){}
+    public void drawCard(){}
+    public void shuffleCards(){}
+
 }
