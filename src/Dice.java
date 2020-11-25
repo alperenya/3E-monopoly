0974e57a17ac3ec5000cab940231c41aaaa6b550
kiraci <osmanbugraaydin@gmail.com>
2020-11-26 00:32:37 +0300

    Dice class has been added.

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,14 @@
 public class Dice {
 
+    //properties
+    private int value;
+
+    //constructers
+    public Dice(){}
+
+    //methods
+    public int roll(){
+        return (int)( Math.random() * 6 + 1 );
+    }
+
 }
