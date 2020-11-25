12194ddcc44e4193bdaf3029370161041f3cdcd1
kiraci <osmanbugraaydin@gmail.com>
2020-11-25 23:49:48 +0300

    BeInfected class has been added.

--- src/BeInfected.java
+++ src/BeInfected.java
@@ -1,2 +1,29 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class BeInfected {
+
+    //properties
+    private int banNumber;
+
+    //constructers
+    public BeInfected(){}
+
+    //methods
+    private Boolean infect(){
+        return true;
+    }
+    private Boolean sendQuarantine(){
+        return true;
+    }
+    public int getBanNumber(){
+        return banNumber;
+    }
+
+    public void setBanNumber(int banNumber) {
+        this.banNumber = banNumber;
+    }
+
+    public Boolean startQuarantine(){
+        return true;
+    }
 }
