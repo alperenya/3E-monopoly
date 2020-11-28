76965eaea487e2a1a123a2f87f585ac0d5e2b619
alperenya <62691122+alperenya@users.noreply.github.com>
2020-11-28 23:19:15 +0300

    player and bot classes changes

--- src/Bot.java
+++ src/Bot.java
@@ -3,20 +3,18 @@ public class Bot extends Player{
     //properties
 
     //contructers
-    public Bot(){}
+    Bot(){}
 
     //methods
     public Boolean decideBuyingProperty(){
         return true;
     }
-    public Boolean decideSellingProperty(){
+    public Boolean decideSellingProperty(Property p){
         return true;
     }
-    public Boolean decideBuilding(){
+    public Boolean decideBuilding(Property p){
         return true;
     }
-    public Boolean trade(){
-        return true;
-    }
+    public Boolean decideTrading(Player p){return true; }
 
 }
