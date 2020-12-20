e520ec5100faeb355482888b9c63266a72b81f52
eaktas2 <eaktas2@gmail.com>
2020-12-20 19:54:21 +0300

    Added Cell Javadoc Comments

--- src/sample/Model/Cell.java
+++ src/sample/Model/Cell.java
@@ -24,31 +24,48 @@ public abstract class Cell {
         return name;
     }
 
+    /**
+     * Sets the name of the Cell
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This method is used to get the visitors of a specific sell
+     * @return ArrayList<Player> Returns the list of players on a cell
+     */
     public ArrayList<Player> getVisitors() {
         return visitors;
     }
 
+    /**
+     * This method is used to get the individuals Piece
+     * @return ArrayList<Pane> Returns the list of pieces
+     */
     public ArrayList<Pane> getVisitorsPiece(){
         ArrayList<Pane> pieces = new ArrayList<>();
-       // if(visitors != null){
             for (Player p:visitors) {
                 pieces.add(p.getPiece());
             }
-       // }
-
         return pieces;
     }
 
+    /**
+     * This method is used to set the vitors of a cell
+     * @param visitors
+     */
     public void setVisitors( ArrayList<Player> visitors) {
         this.visitors = (ArrayList<Player>) visitors.clone();
     }
 
-    public void addVisitor(Player p){
-        visitors.add(p);
+    /**
+     * This method is used to add visitor to an cell
+     * @param addedPlayer
+     */
+    public void addVisitor(Player addedPlayer){
+        visitors.add(addedPlayer);
     }
 
     public double getX(){
